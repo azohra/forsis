aa1b7f0992685d24ced50b0a8f23e0fdcb9a5332
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2018-12-18 16:01:59 -0500

    added a test to ensure that raises an error if mandatory options are not provided in the action

--- spec/sonar_test_report_action_spec.rb
+++ spec/sonar_test_report_action_spec.rb
@@ -1,9 +1,19 @@
 describe Fastlane::Actions::SonarTestReportAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The sonar_test_report plugin is working!")
+  describe 'handles invalid data' do
+    
+    it 'raises an error if mandatory options are not provided in the action' do
 
-      Fastlane::Actions::SonarTestReportAction.run(nil)
-    end
+      # Fastlane::Actions::SonarTestReportAction.run(nil)
+      # expect(Fastlane::UI).to receive(:message).with("sonar_test_report action missing these keys:junit_report")
+      lane = "lane :test do
+      sonar_test_report(
+        sonar_generated_report: 'fastlane/Test-report.xml'
+      )
+      end"
+      expect do
+        Fastlane::FastFile.new.parse(lane).runner.execute(:test).to raise_error("sonar_test_report action missing these keys: junit_report")
+      end
+   end
+
   end
 end
